41260bba7e93d77162dfb32d8e8a74d231408b9c
Andres Buitrago <andresbv0620@hotmail.com>
2016-01-29 01:34:35 -0500

    gitignore working in all directories and new structure to production

--- colector/wsgi.py
+++ colector/wsgi.py
@@ -9,11 +9,17 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-#Se agrega para servir static files
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colector.settings")
 
-application = get_wsgi_application()
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+
+#Se agrega para servir static files
+from dj_static import Cling
+application = Cling(get_wsgi_application())
+
+#comentar para usar dj_static
+#application = get_wsgi_application()

fa3ce8be5a4d15462af4df2a9f0179cec66bcce7
Andres Buitrago <andresbv0620@hotmail.com>
2016-02-02 03:15:08 -0500

    Rollback to standar django files and settings structure

--- colector/wsgi.py
+++ colector/wsgi.py
@@ -1,25 +1,14 @@
 """
 WSGI config for colector project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colector.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-
-#Se agrega para servir static files
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
-
-#comentar para usar dj_static
 application = get_wsgi_application()

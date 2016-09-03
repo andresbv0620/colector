4c28ed80f53b0947a3f30d9d4bbb28d1aec416ad
Andres Buitrago <andresbv0620@hotmail.com>
2016-09-03 16:09:14 -0500

    media url

--- service/views.py
+++ service/views.py
@@ -831,7 +831,7 @@ class FillResponsesForm(View):
                     if entrada.tipo == '6' or entrada.tipo=='14' or entrada.tipo=='16':
                         #src='/home/andres/media/'+response['value']
                         #src='https://s3-us-west-2.amazonaws.com/colector.co/media/'+str(entrada.id)+'/'+response['value']
-                        src=settings.FILES_ROOT+str(entrada.id)+'/'+response['value']
+                        src=settings.MEDIA_URL+str(entrada.id)+'/'+response['value']
                         print src
                         static_url=settings.STATIC_URL
                         if response['label'] in rows:

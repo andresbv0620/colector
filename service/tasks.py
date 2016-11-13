ce2c90cd289b5acc711e4b9d32958b79f7e1ca76
Andres Buitrago <andresbv0620@hotmail.com>
2016-11-12 20:42:47 -0500

    Organize excel report

--- service/tasks.py
+++ service/tasks.py
@@ -35,37 +35,54 @@ def generate_xls_report(id, email):
     if filled_forms.count() != 0:
         rows = []  # rows array que contiene las filas de la tabla
         # Below f is a document (a record)
+
+        workbook = xlsxwriter.Workbook('reporttq.xlsx')
+        worksheet = workbook.add_worksheet()
+
+        # Add a bold format to use to highlight cells.
+        bold = workbook.add_format({'bold': True})
+
+        # Add a number format for cells with money.
+        money = workbook.add_format({'num_format': '$#,##0'})
+
+        # Start from the first cell. Rows and columns are zero indexed.
+        rownumber = 1
+        col = 0
         for f in filled_forms:
             f["rows"]["MongoId"] = str(f["_id"])
             # rows.append(f["rows"])#list of records
             mongoid = str(f["_id"])
 
-            ############ ESTO DEMUESTRA QUE SE PUEDE SIMPLIFICAR EL SERVICIO PARA SINCRONIZAR REGISTROS, ESTA CARGA SE PUEDE PASAR AQUI
-            # ## LA OTRA FORMA DE HACERLO, ES CONSULTAR DIRECTAMENTE EL NODO ROWS
             row = f["rows"]
             formulario = registro_models.Formulario.objects.get(id=int(id))
             row['form_name'] = formulario.nombre
             row['form_description'] = formulario.descripcion
             for response in f["responses"]:
+
+                if response['label'] == 'latitud' or response['label'] == 'longitud' \
+                    or response['label'] == 'form_id' or response['label'] == 'form_description' \
+                    or response['label'] == 'MongoId' or response['label'] == 'Hora Inicio' or response['label'] == 'Hora Fin' \
+                    or response['label'] == 'record_id' or response['label'] == 'sincronizado_utc' or response['label'] == 'colector_id':
+                    continue
                 # input_id=response['input_id']
                 # entrada = Entrada.objects.get(id = int(input_id))
                 # response['label']=entrada.nombre
                 # response['tipo']=entrada.tipo
                 if response['tipo'] == "1" or response['tipo'] == "2":
-                    row[response['label']] = response['value'].upper()
+                    cellvalue = response['value'].upper()
 
                 if response['tipo'] == "3" or response['tipo'] == "4" or response['tipo'] == "5":
                     try:
                         response_id = response['value']
                         respuesta = registro_models.Respuesta.objects.get(id=int(response_id))
-                        row[response['label']] = respuesta.valor
+                        cellvalue = respuesta.valor
                     except Exception, e:
-                        row[response['label']] = "Op_" + response['value']
+                        cellvalue = "Op_" + response['value']
 
                 if response['tipo'] == "7" or response['tipo'] == "8" or response['tipo'] == "9" or response[
                     'tipo'] == "10" or response['tipo'] == "11" or response['tipo'] == "12" or response[
                     'tipo'] == "13" or response['tipo'] == "15" or response['tipo'] == "17":
-                    row[response['label']] = response['value']
+                    cellvalue = response['value']
                 # FOTOS TIENEN UN TAG ADICIONAL A FOTOS Y DOCUMENTOS
                 if response['tipo'] == "6":
                     # src='/home/andres/media/'+response['value']
@@ -76,11 +93,9 @@ def generate_xls_report(id, email):
                     src = settings.MEDIA_URL + str(response['input_id']) + '/' + response['value'] + fileext
                     static_url = settings.STATIC_URL
                     if response['label'] in row:
-                        row[response['label']] = row[response[
-                            'label']] + '<div style="float:left"><a class="thumb"><img onClick="openMedia()" id="' + src + '" width="50px" height="50px" src="' + static_url + 'administrador/admin/dist/img/avatar.png" data-err-src="' + static_url + 'administrador/admin/dist/img/avatar.png"/><p>' + tagfoto + '</p></a></div>'
+                        cellvalue = cellvalue + ' - ' + src
                     else:
-                        row[response[
-                            'label']] = '<div style="float:left"><a class="thumb"><img onClick="openMedia()" id="' + src + '" width="50px" height="50px" src="' + static_url + 'administrador/admin/dist/img/avatar.png" data-err-src="' + static_url + 'administrador/admin/dist/img/avatar.png"/><p>' + tagfoto + '</p></a></div>'
+                        cellvalue = src
 
                 if response['tipo'] == "14" or response['tipo'] == "16":
                     # src='/home/andres/media/'+response['value']
@@ -91,64 +106,24 @@ def generate_xls_report(id, email):
                     src = settings.MEDIA_URL + str(response['input_id']) + '/' + response['value'] + fileext
                     static_url = settings.STATIC_URL
                     if response['label'] in row:
-                        row[response['label']] = row[response[
-                            'label']] + '<div style="float:left"><a class="thumb"><img onClick="openMedia()" id="' + src + '" width="50px" height="50px" src="' + static_url + 'administrador/admin/dist/img/avatar.png" data-err-src="' + static_url + 'administrador/admin/dist/img/avatar.png"/></a></div>'
+                        cellvalue = cellvalue + ' - ' + src
                     else:
-                        row[response[
-                            'label']] = '<div style="float:left"><a class="thumb"><img onClick="openMedia()" id="' + src + '" width="50px" height="50px" src="' + static_url + 'administrador/admin/dist/img/avatar.png" data-err-src="' + static_url + 'administrador/admin/dist/img/avatar.png"/></a></div>'
+                        cellvalue = src
+
+                # Adjust the column width.
+                worksheet.set_column(col, col, 30)
+                #Escribo el encabezado
+                if rownumber-1 == 0:
+                    worksheet.write(rownumber-1, col, response['label'], bold)
+                worksheet.write(rownumber, col, cellvalue)
+                col += 1
+            rownumber += 1
+            col = 0
 
             rows.append(row)  # list of records
 
-    # else:
-    #     print 'NO HAY REGISTROS'
-    #     data = {}
-    #     data['response_code'] = '404'
-    #     data['response_description'] = 'No hay registros'
-    #     data['rows'] = []
-    #     data['total'] = 0
-    #     return HttpResponse(json.dumps(data, default=json_util.default), content_type='application/json')
+        workbook.close()
 
-    # Create a workbook and add a worksheet.
-    exceltimestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    excelfilename = str(id) + '_' + str(exceltimestamp) + '.xlsx'
-
-    workbook = xlsxwriter.Workbook('reporttq.xlsx')
-    worksheet = workbook.add_worksheet()
-
-    # Add a bold format to use to highlight cells.
-    bold = workbook.add_format({'bold': True})
-
-    # Add a number format for cells with money.
-    money = workbook.add_format({'num_format': '$#,##0'})
-
-    # Start from the first cell. Rows and columns are zero indexed.
-    row = 0
-    col = 0
-
-    # Iterate over the data and write it out row by row.
-    for record in rows:
-        for recordnode in record:
-            if recordnode == 'latitud' or recordnode == 'longitud' \
-                    or recordnode == 'form_id' or recordnode == 'form_description' \
-                    or recordnode == 'MongoId' or recordnode == 'Hora Inicio' or recordnode == 'Hora Fin' \
-                    or recordnode == 'record_id' or recordnode == 'sincronizado_utc' or recordnode == 'colector_id':
-                continue
-            # Adjust the column width.
-            worksheet.set_column(col, col, 30)
-            if row == 0:
-                worksheet.write(row, col, recordnode, bold)
-                col += 1
-            else:
-                worksheet.write(row, col, record[recordnode])
-                col += 1
-        row += 1
-        col = 0
-
-    # Write a total using a formula.
-    # worksheet.write(row, 0, 'Total')
-    # worksheet.write(row, 1, '=SUM(B1:B4)')
-
-    workbook.close()
 
     # Connect to s3
     s3_file_name = "reporte.xlsx"
@@ -197,9 +172,7 @@ def generate_xls_report(id, email):
             email.send()
 
 
-    # response = HttpResponse(content_type='application/vnd.ms-excel')
-    # response['Content-Disposition'] = 'attachment; filename=Report.xlsx'
-    # response.write(excelfilename)
+
 
 @shared_task
 def send_record_email(id, email2, email3, responses):

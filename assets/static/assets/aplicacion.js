f4313578f257985a3c1855e18921ac6a54782764
Andres Buitrago <andresbv0620@hotmail.com>
2017-03-10 13:12:44 -0500

    first prototipe colector webforms

--- assets/static/assets/aplicacion.js
+++ assets/static/assets/aplicacion.js
@@ -24,6 +24,10 @@ app.config(['$httpProvider', function($httpProvider) {
 
 app.config(['$routeProvider','globales', function($routeProvider, globales) {
 	$routeProvider
+	.when('/form/:form_id',{
+		controller:"llenarFormulario",
+		templateUrl:globales.static_url+"/webforms/angular_templates/fill_form.html"
+	})
 	.when('/llenar/:form_id',{
 		controller:"llenarFormulario",
 		templateUrl:globales.static_url+"/administrador/angular_templates/llenar_formulario.html"
